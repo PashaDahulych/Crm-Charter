c8b126e76e3589550f2f4dcbb0e693b326fefb16
PashaDahulych <112983354+PashaDahulych@users.noreply.github.com>
2023-12-23 12:36:23 +0200

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -20592,11 +20592,12 @@
         var datepicker_min = __webpack_require__(1448);
         if (document.querySelector("[data-datepicker]")) {
             const picker = datepicker_min("[data-datepicker]", {
-                customDays: [ "Пн", "Вт", "Ср", "Чт", "Пт", "Сб", "Нд" ],
-                customMonths: [ "Січ", "Лют", "Берез", "Квіт", "Трав", "Черв", "Лип", "Серп", "Верес", "Жовт", "Листоп", "Груд" ],
-                overlayButton: "Застосувати",
-                overlayPlaceholder: "Рік (4 цифри)",
-                startDay: 1,
+                customDays: [ "S", "M", "T", "W", "T", "F", "S" ],
+                customMonths: [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ],
+                showAllDates: true,
+                overlayButton: "Apply",
+                overlayPlaceholder: "Year (4 numbers)",
+                startDay: 0,
                 formatter: (input, date, instance) => {
                     const value = date.toLocaleDateString();
                     input.value = value;
@@ -20604,6 +20605,22 @@
                 onSelect: function(input, instance, date) {}
             });
             modules_flsModules.datepicker = picker;
+        }
+        if (document.getElementById("dateCreateSecond")) {
+            const pickerSecond = datepicker_min("#dateCreateSecond", {
+                customDays: [ "S", "M", "T", "W", "T", "F", "S" ],
+                customMonths: [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ],
+                showAllDates: true,
+                overlayButton: "Apply",
+                overlayPlaceholder: "Year (4 numbers)",
+                startDay: 0,
+                formatter: (input, date, instance) => {
+                    const value = date.toLocaleDateString();
+                    input.value = value;
+                },
+                onSelect: function(input, instance, date) {}
+            });
+            modules_flsModules.datepickerSecond = pickerSecond;
         }
         var jquery = __webpack_require__(9755);
         var moment = __webpack_require__(381);
@@ -21617,90 +21634,6 @@
                 }));
             }
         }), 0);
-        class DynamicAdapt {
-            constructor(type) {
-                this.type = type;
-            }
-            init() {
-                this.оbjects = [];
-                this.daClassname = "_dynamic_adapt_";
-                this.nodes = [ ...document.querySelectorAll("[data-da]") ];
-                this.nodes.forEach((node => {
-                    const data = node.dataset.da.trim();
-                    const dataArray = data.split(",");
-                    const оbject = {};
-                    оbject.element = node;
-                    оbject.parent = node.parentNode;
-                    оbject.destination = document.querySelector(`${dataArray[0].trim()}`);
-                    оbject.breakpoint = dataArray[1] ? dataArray[1].trim() : "767";
-                    оbject.place = dataArray[2] ? dataArray[2].trim() : "last";
-                    оbject.index = this.indexInParent(оbject.parent, оbject.element);
-                    this.оbjects.push(оbject);
-                }));
-                this.arraySort(this.оbjects);
-                this.mediaQueries = this.оbjects.map((({breakpoint}) => `(${this.type}-width: ${breakpoint}px),${breakpoint}`)).filter(((item, index, self) => self.indexOf(item) === index));
-                this.mediaQueries.forEach((media => {
-                    const mediaSplit = media.split(",");
-                    const matchMedia = window.matchMedia(mediaSplit[0]);
-                    const mediaBreakpoint = mediaSplit[1];
-                    const оbjectsFilter = this.оbjects.filter((({breakpoint}) => breakpoint === mediaBreakpoint));
-                    matchMedia.addEventListener("change", (() => {
-                        this.mediaHandler(matchMedia, оbjectsFilter);
-                    }));
-                    this.mediaHandler(matchMedia, оbjectsFilter);
-                }));
-            }
-            mediaHandler(matchMedia, оbjects) {
-                if (matchMedia.matches) оbjects.forEach((оbject => {
-                    this.moveTo(оbject.place, оbject.element, оbject.destination);
-                })); else оbjects.forEach((({parent, element, index}) => {
-                    if (element.classList.contains(this.daClassname)) this.moveBack(parent, element, index);
-                }));
-            }
-            moveTo(place, element, destination) {
-                element.classList.add(this.daClassname);
-                if (place === "last" || place >= destination.children.length) {
-                    destination.append(element);
-                    return;
-                }
-                if (place === "first") {
-                    destination.prepend(element);
-                    return;
-                }
-                destination.children[place].before(element);
-            }
-            moveBack(parent, element, index) {
-                element.classList.remove(this.daClassname);
-                if (parent.children[index] !== void 0) parent.children[index].before(element); else parent.append(element);
-            }
-            indexInParent(parent, element) {
-                return [ ...parent.children ].indexOf(element);
-            }
-            arraySort(arr) {
-                if (this.type === "min") arr.sort(((a, b) => {
-                    if (a.breakpoint === b.breakpoint) {
-                        if (a.place === b.place) return 0;
-                        if (a.place === "first" || b.place === "last") return -1;
-                        if (a.place === "last" || b.place === "first") return 1;
-                        return 0;
-                    }
-                    return a.breakpoint - b.breakpoint;
-                })); else {
-                    arr.sort(((a, b) => {
-                        if (a.breakpoint === b.breakpoint) {
-                            if (a.place === b.place) return 0;
-                            if (a.place === "first" || b.place === "last") return 1;
-                            if (a.place === "last" || b.place === "first") return -1;
-                            return 0;
-                        }
-                        return b.breakpoint - a.breakpoint;
-                    }));
-                    return;
-                }
-            }
-        }
-        const da = new DynamicAdapt("max");
-        da.init();
         function showModal(modal) {
             modal.classList.add("_open-modal");
         }
@@ -21784,35 +21717,6 @@
                 }
             }));
         }));
-        document.addEventListener("DOMContentLoaded", (function() {
-            const asideButton = document.querySelector(".aside__button");
-            const aside = document.querySelector(".aside");
-            let isMenuOpen = false;
-            document.addEventListener("click", (function(e) {
-                if (window.innerWidth <= 1310 && aside.contains(e.target)) {
-                    isMenuOpen = true;
-                    document.documentElement.classList.add("menu-open");
-                    document.documentElement.classList.add("lock");
-                } else if (isMenuOpen && e.target !== asideButton) {
-                    isMenuOpen = false;
-                    document.documentElement.classList.remove("menu-open");
-                    document.documentElement.classList.remove("lock");
-                }
-            }));
-            asideButton.addEventListener("click", (function(e) {
-                e.stopPropagation();
-                if (window.innerWidth <= 1310) {
-                    isMenuOpen = !isMenuOpen;
-                    if (isMenuOpen) {
-                        document.documentElement.classList.add("menu-open");
-                        document.documentElement.classList.add("lock");
-                    } else {
-                        document.documentElement.classList.remove("menu-open");
-                        document.documentElement.classList.remove("lock");
-                    }
-                }
-            }));
-        }));
         const menuNotificationOpen = document.querySelector(".header-details__notifications");
         const menuNotificationClosed = document.querySelector(".notification__close");
         if (menuNotificationOpen) menuNotificationOpen.addEventListener("click", (() => {
@@ -21823,7 +21727,7 @@
             document.documentElement.classList.remove("_notifications-open");
             bodyUnlock();
         }));
-        window["FLS"] = true;
+        window["FLS"] = false;
         isWebp();
         spollers();
         formFieldsInit({
